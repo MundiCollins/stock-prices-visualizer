1f266f6fd2f51f6cb636897a9fa573d188f5aa11
MundiCollins <mundicollins@gmail.com>
2019-09-30 07:09:27 +0300

    Fix lint errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,25 @@ export class AppComponent implements OnInit {
     {title: 'Home Depot Inc.', value: 'HD'}, {title: 'The Walt Disney Company', value: 'DIS'},
     {title: 'Microsoft Corporation', value: 'MSFT'}, {title: 'The Boeing Company', value: 'BA'}];
 
+  static stringToDOM(str) {
+    try {
+      const parser = new DOMParser();
+      return parser.parseFromString(str, 'text/html');
+    } catch (error) {
+      throw new Error('Invalid string');
+    }
+  }
+
+  static JSONFromDOM(elem, tag = 'code') {
+    try {
+      let data = elem.getElementsByTagName(tag)[0].innerHTML;
+      data = data.replace(/<a.*<\/a>/, '');  // remove unexpected link tags
+      return JSON.parse(data);
+    } catch (error) {
+      throw new Error('Invalid string');
+    }
+  }
+
   ngOnInit() {
     this.getStockPrices();
   }
@@ -39,24 +58,5 @@ export class AppComponent implements OnInit {
       },
       (error) => console.log('error', error)
     );
-  }
-
-  static stringToDOM(str) {
-    try {
-      const parser = new DOMParser();
-      return parser.parseFromString(str, 'text/html');
-    } catch (error) {
-      throw new Error('Invalid string');
-    }
-  }
-
-  static JSONFromDOM(elem, tag = 'code') {
-    try {
-      let data = elem.getElementsByTagName(tag)[0].innerHTML;
-      data = data.replace(/<a.*<\/a>/, '');  // remove unexpected link tags
-      return JSON.parse(data);
-    } catch (error) {
-      throw new Error('Invalid string');
-    }
   }
 }
